7c72b51852dcdfb1281b57af1cbc47647ca61d84
Diego Zapata <2430053@upv.edu.mx>
2025-10-17 00:45:39 -0600

    Simulador inicial

--- Simulador2D.h
+++ Simulador2D.h
@@ -1,5 +1,8 @@
+#include <iomanip>
+
 template <typename T>
-class Simulador2D {
+class Simulador2D
+{
 private:
     // Matriz de la cuadrícula (Dinámica y Genérica)
     T **_grid;
@@ -8,6 +11,8 @@ private:
 
     // Vector de Fuentes (Dinámico y Genérico)
     T *_fuentes;
+    int *_fuentesFilas;
+    int *_fuentesColumnas;
     int _numFuentes;
     int _capacidadFuentes;
 
@@ -17,9 +22,245 @@ private:
 public:
     // MÉTODOS PÚBLICOS OBLIGATORIOS:
     Simulador2D(int f, int c); // Constructor
-    ~Simulador2D(); // Destructor
+    ~Simulador2D();            // Destructor
     void redimensionarGrid(int nuevaF, int nuevaC);
-    void agregarFuente(T valor); // Expansión manual del vector dinámico si es necesario
-    void simularPaso(); // Implementación del algoritmo
+    void agregarFuente(T valor, int fila, int column); // Expansión manual del vector dinámico si es necesario
+    void simularPaso();                                // Implementación del algoritmo
+
+    void mostrarGrid();
     // ... otros métodos de acceso y visualización
+
+private:
+    void deleteMatriz(T **matriz, int filas, int columnas = 0);
+    T **createMatiz(int filas, int columnas);
+    T **copyMatriz(T **matriz, int filas, int columnas);
+    void redimensionarFuentes();
+
+    // Getters
+public:
+    T **getGrid() { return this->_grid; };
+    int getFilas() { return this->_filas; };
+    int getColumnas() { return this->_columnas; };
 };
+
+// Implementacion
+template <typename T>
+Simulador2D<T>::Simulador2D(int f, int c)
+{
+    // Crear matriz con filas y columnas
+    this->_grid = createMatiz(f, c);
+
+    // Asignar filas y columnas
+    this->_filas = f;
+    this->_columnas = c;
+
+    // Crear vector de fuentes
+    this->_capacidadFuentes = 10;
+    this->_numFuentes = 0;
+    this->_fuentes = new T[this->_capacidadFuentes];
+    this->_fuentesFilas = new int[this->_capacidadFuentes];
+    this->_fuentesColumnas = new int[this->_capacidadFuentes];
+
+    // Definir valor de constantes
+    this->_constantes[0] = 1;
+    this->_constantes[1] = 1;
+    this->_constantes[2] = 1;
+};
+
+template <typename T>
+Simulador2D<T>::~Simulador2D()
+{
+    // Destruir la matriz grid
+    this->deleteMatriz(this->_grid, this->_filas, this->_columnas);
+
+    // Destruir fuentes
+    delete[] this->_fuentes;
+    delete[] this->_fuentesFilas;
+    delete[] this->_fuentesColumnas;
+};
+
+template <typename T>
+void Simulador2D<T>::redimensionarGrid(int nuevaF, int nuevaC)
+{
+    // validar tamaño positivo
+    if (nuevaF < 0 || nuevaC < 0)
+        throw std::invalid_argument("No es posible una medida negativo");
+
+    // 1. Crear nueva matriz e inicializar con valores 0
+    T **nuevaGrid = createMatiz(nuevaF, nuevaC);
+
+    // 2. Copiar valores originales a la nueva matriz
+
+    // 2.1 Definir cual es el limite a copiar
+    int max_cpy_filas = (nuevaF > this->_filas) ? this->_filas : nuevaF;
+    int max_cpy_columnas = (nuevaC > this->_columnas) ? this->_columnas : nuevaC;
+
+    // 2.2 Copiar valores
+    for (int i = 0; i < max_cpy_filas; i++)
+    {
+        for (int j = 0; j < max_cpy_columnas; j++)
+            nuevaGrid[i][j] = this->_grid[i][j];
+    }
+
+    // 4. Eliminar anterior matriz
+    this->deleteMatriz(this->_grid, this->_filas, this->_columnas);
+    this->_grid = nullptr;
+
+    // 5. Remplazamos el puntero
+    this->_grid = nuevaGrid;
+
+    // 6. Despunteamos el auxiliar
+    nuevaGrid = nullptr;
+
+    // 7. actualizamos medidas
+    this->_filas = nuevaF;
+    this->_columnas = nuevaC;
+};
+
+template <typename T>
+void Simulador2D<T>::agregarFuente(T valor, int fila, int columna)
+{
+    if (fila < 0 || fila >= this->_filas || columna < 0 || columna >= this->_columnas)
+    {
+        throw std::out_of_range("Posición de fuente fuera de los límites del grid");
+    }
+    if (this->_numFuentes >= this->_capacidadFuentes)
+    {
+        redimensionarFuentes();
+    }
+    this->_fuentes[this->_numFuentes] = valor;
+    this->_fuentesFilas[this->_numFuentes] = fila;
+    this->_fuentesColumnas[this->_numFuentes] = columna;
+    this->_numFuentes++;
+
+    // Actualizar grid para agregar fuente
+    this->_grid[fila][columna] = valor;
+};
+
+template <typename T>
+void Simulador2D<T>::simularPaso()
+{
+    // Copiar la matriz a una auxiliar
+    T **aux = copyMatriz(this->_grid, this->_filas, this->_columnas);
+
+    for (int i = 1; i < this->_filas - 1; i++)
+    {
+        for (int j = 1; j < this->_columnas - 1; j++)
+        {
+            T sum = this->_grid[i - 1][j];
+            sum += this->_grid[i + 1][j];
+            sum += this->_grid[i][j - 1];
+            sum += this->_grid[i][j + 1];
+
+            aux[i][j] = (sum) / 4;
+        }
+    }
+
+    // Asignar nuevamente los valores de las fuentes
+
+    for (int i = 0; i < this->_numFuentes; i++)
+    {
+        if (this->_fuentesFilas[i] >= this->_filas || this->_fuentesColumnas[i] >= this->_columnas)
+        {
+            continue;
+        }
+        int f = this->_fuentesFilas[i];
+        int c = this->_fuentesColumnas[i];
+        aux[f][c] = this->_fuentes[i];
+    }
+
+    // Eliminar matriz anterior al paso
+    deleteMatriz(this->_grid, this->_filas, this->_columnas);
+
+    // asignar nueva matriz
+    this->_grid = aux;
+};
+template <typename T>
+void Simulador2D<T>::mostrarGrid()
+{
+    for (int i = 0; i < this->_filas; i++)
+    {
+        std::cout << "|";
+        for (int j = 0; j < this->_columnas; j++)
+        {
+            std::cout << std::setw(6) << std::fixed << std::setprecision(2) << this->_grid[i][j] << "|";
+        }
+        std::cout << std::endl;
+    }
+}
+
+template <typename T>
+void Simulador2D<T>::deleteMatriz(T **matriz, int filas, int columnas)
+{
+    for (int i = 0; i < filas; i++)
+        delete[] matriz[i];
+    delete[] matriz;
+}
+
+template <typename T>
+T **Simulador2D<T>::createMatiz(int filas, int columnas)
+{
+    // intentar crear una matriz
+    try
+    {
+        // 1. Crear nueva matriz e inicializar con valores 0
+        T **matriz = new T *[filas];
+        for (int i = 0; i < filas; i++)
+        {
+            matriz[i] = new T[columnas];
+            for (int j = 0; j < columnas; j++)
+                matriz[i][j] = 0;
+        };
+        return matriz;
+    }
+    catch (std::bad_alloc)
+    {
+        throw;
+    }
+};
+template <typename T>
+T **Simulador2D<T>::copyMatriz(T **matriz, int filas, int columnas)
+{
+    T **matriz_cpy = createMatiz(filas, columnas);
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
+        {
+            matriz_cpy[i][j] = matriz[i][j];
+        }
+    }
+    return matriz_cpy;
+}
+
+template <typename T>
+void Simulador2D<T>::redimensionarFuentes()
+{
+    const int newSize = this->_capacidadFuentes * 2;
+
+    // Crear nuevo arreglo
+    T *nuevoFuentes = new T[newSize];
+    int *nuevoFuentesFilas = new int[newSize];
+    int *nuevoFuentesColumnas = new int[newSize];
+
+    // copiar arreglo viejo al nuevo
+
+    for (int i = 0; i < this->_numFuentes; i++)
+    {
+        nuevoFuentes[i] = this->_fuentes[i];
+        nuevoFuentesFilas[i] = this->_fuentesFilas[i];
+        nuevoFuentesColumnas[i] = this->_fuentesColumnas[i];
+    }
+
+    // Eliminar el arreglo veijo
+    delete[] this->_fuentes;
+    delete[] this->_fuentesFilas;
+    delete[] this->_fuentesColumnas;
+
+    // pasar la referencia
+    this->_fuentes = nuevoFuentes;
+    this->_fuentesFilas = nuevoFuentesFilas;
+    this->_fuentesColumnas = nuevoFuentesColumnas;
+
+    // Actualziar el tamaño
+    this->_capacidadFuentes = newSize;
+}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,174 @@
 #include <iostream>
+#include "Simulador2D.h"
+#include <limits>
 
-int main() {
+// Imprimir mensaje
+void print_message(const std::string msg);
+
+// Obtener Inputs de un tipo de dato flexible:
+template <typename T>
+T getInput(const std::string prompt);
+
+template <>
+std::string getInput<std::string>(const std::string prompt);
+
+int get_menu_option();
+void print_menu();
+
+int main()
+{
+    print_message("--- Simulador Generico de Fluidos (Difusion) ---");
+    print_message(">> Inicializando Sistema (Tipo FLOAT) <<");
+    print_message("Creando Grid (FLOAT) de 5x5...");
+    Simulador2D<float> *Grid2d = new Simulador2D<float>(5, 5);
+
+    std::cout << std::endl;
+    print_message(">> Agregando Fuentes de Concentración <<");
+    Grid2d->agregarFuente(100.0, 2, 2);
+    Grid2d->agregarFuente(50.0, 4, 0);
+
+    print_message("Grid[2,2] = 100.0 (Fuente 1)");
+    print_message("Grid[4,0] = 50.0 (Fuente 2)");
+
+    bool isProgramRunning = true;
+    int steps_count = 0;
+    while (isProgramRunning)
+    {
+        int opt = get_menu_option();
+        switch (opt)
+        {
+        case 0:
+        {
+            isProgramRunning = false;
+            break;
+        }
+        case 1:
+        {
+            print_message("Simulando Propagación...");
+            Grid2d->simularPaso();
+            print_message("-- Grid Después del Paso " + std::to_string(++steps_count) + " ---");
+            Grid2d->mostrarGrid();
+            break;
+        }
+        case 2:
+        {
+            int filas = getInput<int>("Filas:");
+            int columnas = getInput<int>("Columnas:");
+            print_message("-- Redimensionando Grid ---");
+            try
+            {
+                Grid2d->redimensionarGrid(filas, columnas);
+            }
+            catch (std::bad_alloc)
+            {
+                print_message("Se te acabo la memoria pai");
+            }
+            Grid2d->mostrarGrid();
+            break;
+        }
+        case 3:
+        {
+            float value = getInput<float>("Value:");
+            int fila = getInput<int>("Fila:");
+            int columna = getInput<int>("Columna:");
+            try
+            {
+
+                Grid2d->agregarFuente(value, fila, columna);
+            }
+            catch (std::exception &e)
+            {
+                print_message("Error :" + std::string(e.what()));
+            }
+            break;
+        }
+        }
+    }
+
+    print_message("Liberando memoria del Grid y Fuentes... Sistema cerrado.");
+    // liberando recursos
+    delete Grid2d;
+
     return 0;
+}
+
+int get_menu_option()
+{
+    bool isValidOption = false;
+    int opt;
+    do
+    {
+        print_menu();
+        opt = getInput<int>("Ingrese una opcion [0-3]:");
+        if (opt < 0 || opt > 3)
+        {
+            std::cout << "[Error] Ingrese una opcion valida" << std::endl;
+            continue;
+        }
+        isValidOption = true;
+    } while (!isValidOption);
+    return opt;
+}
+
+void print_menu()
+{
+    const std::string MENU_TOP_ROUND = "╭───────────────{ ✦ MENÚ PRINCIPAL ✦ }────────────────╮";
+    const std::string MENU_BORDER = "│";
+    const std::string MENU_BOT_ROUND = "╰─────────────────────────────────────────────────────╯";
+    const int content_width = 53;
+    int options_count = 3;
+    std::string options[] = {
+        "Simular Paso",
+        "Redimensionar",
+        "Agregar fuente"};
+
+    std::cout << MENU_TOP_ROUND << std::endl;
+    // for each option print in menu
+    for (int i = 0; i < options_count; i++)
+    {
+        // create option line
+        std::string option = " " + std::to_string(i + 1) + ". " + options[i];
+        std::cout << MENU_BORDER << std::left << std::setw(content_width) << option << MENU_BORDER << std::endl;
+    }
+    // add leave at the end
+    std::cout << MENU_BORDER << std::left << std::setw(content_width) << " 0. Salir" << MENU_BORDER << std::endl;
+
+    std::cout << MENU_BOT_ROUND << std::endl;
+}
+
+template <typename T>
+T getInput(const std::string prompt)
+{
+    T input;
+    std::cout << prompt;
+    while (!(std::cin >> input))
+    {
+        std::cout << "Ingrese un input valido!" << std::endl;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << prompt;
+    }
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    return input;
+}
+
+template <>
+std::string getInput(const std::string prompt)
+{
+    std::cout << prompt;
+    std::string temp;
+    std::getline(std::cin, temp);
+    return temp;
+}
+
+void print_message(const std::string msg)
+{
+    const std::string MENU_TOP_ROUND = "╭─────────────────────────────────────────────────────╮";
+    const std::string MENU_BORDER = "│";
+    const std::string MENU_BOT_ROUND = "╰─────────────────────────────────────────────────────╯";
+    const int content_width = 50;
+    std::cout << MENU_TOP_ROUND << std::endl;
+    std::string formatted_msg = " " + msg + " ";
+    std::cout << MENU_BORDER << std::left << std::setw(content_width) << formatted_msg << std::endl;
+    std::cout << MENU_BOT_ROUND << std::endl;
 }
